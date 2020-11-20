44e021be40004aadabdd411a31b409b6c9dc3731
Shrutika252 <71875791+Shrutika252@users.noreply.github.com>
2020-11-20 12:26:41 +0530

    Create Size.cpp

---
+++ Size.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	int a;
+	float b;
+	double d;
+	
+	std::cout<<"Size of char :"<<sizeof(ch)<<endl;
+		std::cout<<"Size of int :"<<sizeof(a)<<endl;
+			std::cout<<"Size of float :"<<sizeof(b)<<endl;
+				std::cout<<"Size of double :"<<sizeof(d)<<endl;
+	
+	return 0;
+}

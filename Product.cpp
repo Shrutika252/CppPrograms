e97244a19d770aefc2affbad6b8ffe09beec5a73
Shrutika252 <71875791+Shrutika252@users.noreply.github.com>
2020-11-20 12:35:26 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	std::cout<<"Enter Two Number To Find The Product:"<<endl;
+	std::cin>>a>>b;
+	std::cout<<"Product is:"<<(a*b);
+	
+	
+	return 0;
+}
+	
